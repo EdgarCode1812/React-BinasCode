244623e793310b76b9fd1ca748113dc63c1258e0
React-BinasCode <edgar.cortes1812@gmail.com>
2023-05-04 17:00:55 -0600

    Se agrego envio de correos

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -2,47 +2,53 @@ import React from 'react'
 import './Contacto.css'
 
 export default function Contacto() {
-  return (
-    <div>
-           <section id="contacto" className="contacto">
-        <div className="contenido-seccion">
-            <h2>CONTACTO</h2>
-            <div className="fila">
+    return (
+        <div>
+            <section id="contacto" className="contacto">
+                <div className="contenido-seccion">
+                    <h2>CONTACTO</h2>
+                    <div className="fila">
+                        <div className="col">
 
-                <div className="col">
-                    <input type="text" placeholder="Tú Nombre"/>
-                    <input type="text" placeholder="Número telefónico"/>
-                    <input type="text" placeholder="Dirección de correo"/>
-                    <input type="text" placeholder="Tema"/>
-                    <textarea name="" id="" cols="30" rows="6" placeholder="Mensaje"></textarea>
-                    <button>
-                        Enviar Mensaje <i className="fa-solid fa-paper-plane"></i>
-                        <span className="overlay"></span>
-                    </button>
-                </div>
-     
-                <div className="col">
-                    <img src="/img/ubicacion.png" alt=""/>
-                    <div className="info">
-                        <ul>
-                            <li>
-                                <i className="fa-solid fa-location-dot"></i>
-                                Córdoba, Veracruz
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-mobile-screen"></i>
-                                Llamame: +52 271 124 5170
-                            </li>
-                            <li>
-                                <i className="fa-solid fa-envelope"></i>
-                                Email: binascode@gmail.com
-                            </li>
-                        </ul>
+
+
+                            <form action="https://formsubmit.co/binascode@gmail.com" method="POST">
+                                <input type="text" name="name" placeholder="Tú Nombre" />
+                                <input type="text" name="phone" placeholder="Número telefónico" />
+                                <input type="email" name="email" placeholder="Dirección de correo" />
+                                <textarea id="" cols="30" rows="6" name="mensage" placeholder="Mensaje"></textarea>
+                                <button type="submit">
+                                    Enviar Mensaje <i className="fa-solid fa-paper-plane"></i>
+                                    <span className="overlay"></span>
+                                </button>
+                                <input type="hidden" name="_next" value="http://127.0.0.1:5173/" />
+                                <input type="hidden" name="_captcha" value="false" />
+
+                            </form>
+
+                        </div>
+                        <div className="col">
+                            <img src="/img/ubicacion.png" alt="" />
+                            <div className="info">
+                                <ul>
+                                    <li>
+                                        <i className="fa-solid fa-location-dot"></i>
+                                        Córdoba, Veracruz
+                                    </li>
+                                    <li>
+                                        <i className="fa-solid fa-mobile-screen"></i>
+                                        Llamame: +52 271 124 5170
+                                    </li>
+                                    <li>
+                                        <i className="fa-solid fa-envelope"></i>
+                                        Email: binascode@gmail.com
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
-    </section>
-    </div>
-  )
+    )
 }
